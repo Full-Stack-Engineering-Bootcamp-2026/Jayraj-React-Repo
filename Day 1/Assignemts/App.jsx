ec29b9562a0c3129cc6626a7ef2112f07d9e3f8b
Jayraj Gajul <jayraj.gajul@mindbowser.com>
2026-03-17 17:19:16 +0530

    feat:add day 1 assignment

---
+++ Day 1/Assignemts/App.jsx
@@ -0,0 +1,29 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from './assets/vite.svg'
+import heroImg from './assets/hero.png'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+  const name ="Jayraj Venugopal Gajul";
+  const batch ="Full Stack Bootcamp";
+    
+  return (
+    <>
+    <div className="container">
+      <h1 style={{color:'blue'}}>{name}</h1>
+      <p>Batch - {batch}</p>
+      <p>Current Date & Time : {new Date().toLocaleString()}</p>
+
+      <ul>
+        <li>Learing React</li>
+        <li>Using JSX element</li>
+        <li>Applying styles</li>
+      </ul>
+    </div>
+    </>
+  )
+}
+
+export default App
