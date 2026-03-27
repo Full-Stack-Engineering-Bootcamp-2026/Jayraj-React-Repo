e4e5b89f20b1377d25a0c6b490aae99c4bc67f6f
Jayraj Gajul <jayraj.gajul@mindbowser.com>
2026-03-27 14:16:49 +0530

    feat:added day 8 assignment

---
+++ Day 8/my-react-project/src/App.jsx
@@ -0,0 +1,29 @@
+import Button from "./Button";
+
+function App(){
+
+  return(
+    <div>Hiii
+
+      <div>
+        <Button success primary rounded outline >Click!</Button>
+      </div>
+      <div>
+        <Button>Buy Now</Button>
+      </div>
+      <div>
+        <Button>Hide App</Button>
+      </div>
+      <div>
+        <Button>Close</Button>
+      </div>
+      <div>
+        <Button>Yoooo</Button>
+      </div>
+      
+    </div>
+  
+  )
+}
+
+export default App;
---
+++ Day 8/my-react-project/src/Button.jsx
@@ -0,0 +1,34 @@
+import PropTypes from "prop-types";
+
+function Button( { 
+    children,
+    primary,
+    secondary,
+    success,
+    warning,
+    danger,
+    outline,
+    rounded,
+ } ){
+
+
+    return <button>{children}</button>
+}
+
+Button.propType = {
+    checkVariationValue: ({ primary, secondary, success, warning, danger}) => {
+        const count = Number(!!primary)
+            + Number(!!secondary)
+            + Number(!!warning)
+            + Number(!!success)
+            + Number(!!danger)
+
+        if(count>1){
+            return new Error(
+                'Only one should be procided'
+            )
+        }
+    }
+};
+
+export default Button;
